78c64095227cb532303a48bfba876cf7046b8eeb
Nectarios Chroniaris <nchroniaris@gmail.com>
2020-11-23 14:48:14 -0700

    initial project commit

---
+++ src/main/java/com/nchroniaris/ucinjector/Main.java
@@ -0,0 +1,9 @@
+package com.nchroniaris.ucinjector;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
---
+++ src/test/java/com/nchroniaris/ucinjector/MainTest.java
@@ -0,0 +1,16 @@
+package com.nchroniaris.ucinjector;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void mainTest() {
+
+        // This is just a test test to see if tests work in Gradle
+        Assert.assertTrue(true);
+
+    }
+
+}
