1fad052c72923ce72f386b641436593ac2d9c933
Nectarios Chroniaris <nchroniaris@gmail.com>
2020-11-24 11:38:06 -0700

    add method to find working dir of jar

--- src/main/java/com/nchroniaris/ucinjector/Main.java
+++ src/main/java/com/nchroniaris/ucinjector/Main.java
@@ -1,6 +1,38 @@
 package com.nchroniaris.ucinjector;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 public class Main {
+
+    /**
+     * Uses the location of the Main class as a reference to obtain the path to the directory that encloses the jar file being run. We must decode the path as a URL as the presence of any spaces in the path will result in a `%20` instead of an actual space.
+     *
+     * @return The full path (spaces are unescaped) of the directory that the .jar resides in.
+     */
+    public static String findJarWorkingDir() {
+
+        // Adapted from: https://stackoverflow.com/questions/40317459/how-to-open-a-file-in-the-same-directory-as-the-jar-file-of-the-application
+
+        // Get the location of the jar file, and then represent it as a file to get its parent.
+        String dir = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+
+        // Try to decode the URL-esque string we get above. This is to remove spaces and other special URL characters.
+        try {
+
+            dir = URLDecoder.decode(dir, "UTF-8");
+
+        } catch (UnsupportedEncodingException e) {
+
+            System.err.println("[CRITICAL] Something went wrong with finding the location of the .jar file: " + e.getMessage());
+            System.exit(1);
+
+        }
+
+        return dir;
+
+    }
 
     public static void main(String[] args) {
         System.out.println("test");
